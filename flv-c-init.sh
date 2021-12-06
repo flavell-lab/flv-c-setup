345d6a38c694025a7e064bc2e8f66d432a8bbf28
Jungsoo Kim <urlicht@users.noreply.github.com>
2021-12-06 16:17:47 -0500

    Update flv-c-init.sh
    fix typo with PyTorch-3dunet

--- flv-c-init.sh
+++ flv-c-init.sh
@@ -34,7 +34,7 @@ cd ~/src
 rm ~/src/pytorch-3dunet
 git clone git@github.com:flavell-lab/pytorch-3dunet
 cd ~/src/pytorch-3dunet
-pip install.
+pip install .
 
 # other packages
 julia -e "import Pkg; pkg = Pkg.PackageSpec(name=\"FlavellPkg\", url=\"git@github.com:flavell-lab/FlavellPkg.jl.git\", rev=\"develop\"); Pkg.add(pkg)"
