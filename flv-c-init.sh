5b41e325cc95383d86d21e5beeb3272e1dc67740
candy <64230533+dikang13@users.noreply.github.com>
2024-06-10 13:44:49 -0400

    Update flv-c-init.sh
    specified sh interpreter;
    fixed bugs in for loop that caused confusion with the sh interpreter;
    removed the last line since /tmp/installer.sh is always not found.

--- flv-c-init.sh
+++ flv-c-init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ##### src dir
 date_str=$(date +'%Y-%m-%d')
@@ -16,22 +16,21 @@ julia -e "import Pkg; ENV[\"PYTHON\"]=\"\";
 # update conda
 conda update --all -y
 
-# public py libraries - torch, tensorflow, cuda
+# public py libraries - torch, tensorflow
 pip install torch torchvision torchaudio 
 python3 -m pip install tensorflow==2.15
-python3 -m pip install cuda
 
 # private fork of public library - deepreg
 # private py packages - unet2d, euler_gpu, autolabel
 # src code will be erased from home directory after installation
-declare -a repos=(unet2d euler_gpu DeepReg autolabel)
+repos="unet2d euler_gpu DeepReg autolabel"
 mkdir -p $path_dir_src_temp
-for repo in "${repos[@]}"; do
+for repo in $repos; do
     cd $path_dir_src_temp
-    rm -rf $path_dir_src_temp/$repo  # Remove the existing repository directory, if it exists
-    git clone git@github.com:flavell-lab/$repo.git
-    cd $path_dir_src_temp/$repo    
-    if [ $repo == unet2d ]; then  # For 'unet2d', checkout a specific version
+    rm -rf "$path_dir_src_temp/$repo"  # Remove the existing repository directory, if it exists
+    git clone "git@github.com:flavell-lab/$repo.git" "$path_dir_src_temp/$repo"
+    cd "$path_dir_src_temp/$repo" 
+    if [ "$repo" = "unet2d" ]; then
         git checkout v0.1
     fi
     pip install .
@@ -47,7 +46,7 @@ pip install .
 
 #### julia packages
 julia -e "import Pkg; Pkg.add(\"PyPlot\")" # pyplot
-julia -e "import Pkg; pkg = Pkg.PackageSpec(name=\"FlavellPkg\", url=\"git@github.com:flavell-lab/FlavellPkg.jl.git\"); Pkg.add(pkg)"
+julia -e "using Pkg; pkg = Pkg.PackageSpec(name=\"FlavellPkg\", url=\"git@github.com:flavell-lab/FlavellPkg.jl.git\"); Pkg.add(pkg)"
 julia -e "using Pkg; pkg = Pkg.PackageSpec(name=\"FlavellPkg\", url=\"git@github.com:flavell-lab/FlavellPkg.jl.git\", rev=\"dev\"); Pkg.add(pkg)" #temporary
 julia -e "using FlavellPkg; FlavellPkg.install_default();"
 julia -e "using FlavellPkg; FlavellPkg.install_ANTSUN(false);"
@@ -61,4 +60,3 @@ julia -e "using IJulia; IJulia.installkernel(\"Julia\")"
 
 # remove temp src dir
 rm -rf $path_dir_src_temp
-rm /tmp/installer.sh
