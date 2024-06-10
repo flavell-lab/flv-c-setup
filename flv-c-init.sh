bde36416c8a767a41214dea070201da417a81875
candy <64230533+dikang13@users.noreply.github.com>
2024-06-10 12:34:21 -0400

    Update flv-c-init.sh
    added a temporary line that installs the #dev branch of FlavellPkg.jl, will remove it after testing

--- flv-c-init.sh
+++ flv-c-init.sh
@@ -48,6 +48,7 @@ pip install .
 #### julia packages
 julia -e "import Pkg; Pkg.add(\"PyPlot\")" # pyplot
 julia -e "import Pkg; pkg = Pkg.PackageSpec(name=\"FlavellPkg\", url=\"git@github.com:flavell-lab/FlavellPkg.jl.git\"); Pkg.add(pkg)"
+julia -e "using Pkg; pkg = Pkg.PackageSpec(name=\"FlavellPkg\", url=\"git@github.com:flavell-lab/FlavellPkg.jl.git\", rev=\"dev\"); Pkg.add(pkg)" #temporary
 julia -e "using FlavellPkg; FlavellPkg.install_default();"
 julia -e "using FlavellPkg; FlavellPkg.install_ANTSUN(false);"
 julia -e "using FlavellPkg; FlavellPkg.install_CePNEM(false);"
